0954f0f84b31ceb3f37ec25a67ee7ccf5f038e13
McodexD <103727982+McodexD@users.noreply.github.com>
2024-09-19 19:51:29 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+<script src="https://kit.fontawesome.com/bead6e030d.js" crossorigin="anonymous"></script>
+document.getElementById('contactForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Thank you for contacting me, I will respond shortly!');
+});
